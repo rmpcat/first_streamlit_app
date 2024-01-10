78002bdcd6edd4ffc28fc3ae41c214caa9259543
rmpcat <156185592+rmpcat@users.noreply.github.com>
2024-01-10 10:40:01 +0000

    Update streamlit_app.py
    update smoothie fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,12 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Cantaloupe', 'Grapefruit'])
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = mmy_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
 
 
 
